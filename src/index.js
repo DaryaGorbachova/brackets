01990abe279d4f296bd57aadb17001b46c99ff3b
DaryaGorbachova <vrindavaneshvari@mail.ru>
2021-08-29 11:37:01 +0300

    count of brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let count1 = 0;
+	let count2 = 0;
+  for (let i = 0; i < str.length; i++) {
+	  for (let j = 0; j < bracketsConfig.length; j++) {
+	if(str[i] === bracketsConfig[j][0]) count1++;
+	if(str[i] === bracketsConfig[j][1]) count2++;
+	  }
+  	
+  }
+	if (count1 === count2) return true
+	else return false
 }
