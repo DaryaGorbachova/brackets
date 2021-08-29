3a05c2ae8ac48599d6d129d804600b54d9d99147
DaryaGorbachova <vrindavaneshvari@mail.ru>
2021-08-29 11:59:17 +0300

    no reverse brackets

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-	let count1 = 0;
-	let count2 = 0;
+	let array1 = [];
+	let array2 = [];
   for (let i = 0; i < str.length; i++) {
 	  for (let j = 0; j < bracketsConfig.length; j++) {
-	if(str[i] === bracketsConfig[j][0]) count1++;
-	if(str[i] === bracketsConfig[j][1]) count2++;
-	  }
+	if(str[i] === bracketsConfig[j][0]) {
+		array1.push(i);
+	}
+	if(str[i] === bracketsConfig[j][1]) {
+		array2.push(i);
+	}
+  }
   	
   }
-	if (count1 === count2) return true
+	if (array1.length === array2.length) {
+	//	console.log(array2)
+		for (let i = 0; i < array1.length; i++) {
+			if(array1[i] > array2[i]) return false
+		}
+		return true
+	}
 	else return false
 }
